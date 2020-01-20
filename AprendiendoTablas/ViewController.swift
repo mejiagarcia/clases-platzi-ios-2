5318922a4eb65625fad01eb3c86621a019c47222
Carlos Mejia <carlosmejia083@gmail.com>
2020-01-20 17:13:00 -0500

    Clase 8 - UITableView

--- AprendiendoTablas/ViewController.swift
+++ AprendiendoTablas/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Referencia de la tabla
+    @IBOutlet weak var tableView: UITableView!
+    
+    /*
+     1. Implementar DataSource <- interfaz
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // No olvidar!! :)
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mi-celda")
+        
+        // Para interactuar, implementar el delegate
+        tableView.delegate = self
+        
     }
-
-
 }
 
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("La celda #\(indexPath.row) fué seleccionada.")
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    // 1. Número de filas que tendrá nuestra tabla
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    // 2. Método para saber que celdas deben mostrarse.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mi-celda", for: indexPath)
+        
+        cell.textLabel?.text = "Soy la celda #\(indexPath.row)"
+        
+        return cell
+    }
+}
